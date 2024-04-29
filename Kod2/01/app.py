0d7960f6318a439bfdc3d152e62b257e9f741ec1
Kiss, Tibor <tibor.kiss01@sap.com>
2024-04-29 16:27:07 +0200

    Teszt

--- Kod2/01/app.py
+++ Kod2/01/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Kezdőoldal
 @app.route("/", methods=["GET"])
 def info():
-    return "Docker webalkalmazás"
+    return "Docker alkalmazás"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
